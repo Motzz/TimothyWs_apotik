46450e78a18e4dcb315afccc17b2c98e9766e89c
Motzz <s160419007@student.ubaya.ac.id>
2022-02-23 19:52:19 +0700

    160419007_Apotik

---
+++ database/migrations/2022_02_23_115823_add_column_medicines.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnMedicines extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+         Schema::table('medicines', function (Blueprint $table) {
+            //
+            $table->string("form",200);
+            $table->string("formula",200);
+            $table->string("description",200);
+            $table->boolean('faskes_1')->default(0);
+            $table->boolean('faskes_2')->default(0);
+            $table->boolean('faskes_3')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+         Schema::table('medicines', function (Blueprint $table) {
+            //
+            $table->dropColumn("form",200);
+            $table->dropColumn("formula",200);
+            $table->dropColumn("description",200);
+            $table->dropColumn('faskes_1')->default(0);
+            $table->dropColumn('faskes_2')->default(0);
+            $table->dropColumn('faskes_3')->default(0);
+        });
+    }
+}
